486eef097751d031fcdc2e8962ff990d44ea25b0
k1m0ch1 <yahya.kimochi@gmail.com>
2020-11-27 23:20:56 +0700

    fix: notifier will exit when there is no yesterday file exist

--- notifier.py
+++ notifier.py
@@ -1,12 +1,19 @@
 import os
+import sys
 import json
 import requests
 from datetime import date, timedelta
+
+
 
 WEBHOOK_URL = "https://discordapp.com/api/webhooks/781911618702409748/QQ89_tlwWYuT-fUSEkUnPMmJDlH2ohn6rp8bbcIEhL2cHX_8fbLdtE5UGLZ4VbU4fjnR"
 DATA_DIR = './data'
 TODAY_STRING = date.today().strftime("%Y-%m-%d")
 YESTERDAY_STRING = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+if not os.path.isfile(f'{DATA_DIR}/{YESTERDAY_STRING}.json'):
+    sys.exit(0)
+
 TODAY_FILE = open(f"{DATA_DIR}/{TODAY_STRING}.json", 'r').read()
 YESTERDAY_FILE = open(f"{DATA_DIR}/{YESTERDAY_STRING}.json", 'r').read()
 
