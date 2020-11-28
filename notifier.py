07cf474067bc4b80dc8bf8d69c32fadd12b3aa6b
k1m0ch1 <yahya.kimochi@gmail.com>
2020-11-29 01:11:20 +0700

    add: pagination data so every data in hotdeals is recorded

--- notifier.py
+++ notifier.py
@@ -4,41 +4,39 @@ import json
 import requests
 from datetime import date, timedelta
 
-print("Run the Notifier")
+def RunNotifier():
+    print("Run the Notifier")
 
-WEBHOOK_URL = "https://discordapp.com/api/webhooks/781911618702409748/QQ89_tlwWYuT-fUSEkUnPMmJDlH2ohn6rp8bbcIEhL2cHX_8fbLdtE5UGLZ4VbU4fjnR"
-DATA_DIR = './data'
-TODAY_STRING = date.today().strftime("%Y-%m-%d")
-YESTERDAY_STRING = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+    WEBHOOK_URL = "https://discordapp.com/api/webhooks/781911618702409748/QQ89_tlwWYuT-fUSEkUnPMmJDlH2ohn6rp8bbcIEhL2cHX_8fbLdtE5UGLZ4VbU4fjnR"
+    DATA_DIR = './data'
+    TODAY_STRING = date.today().strftime("%Y-%m-%d")
+    YESTERDAY_STRING = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
-if not os.path.isfile(f'{DATA_DIR}/{YESTERDAY_STRING}.json'):
-    print("There is no yesterday data file, you need to wait more")
-    sys.exit(0)
+    if not os.path.isfile(f'{DATA_DIR}/{YESTERDAY_STRING}.json'):
+        print("There is no yesterday data file, you need to wait more")
+        sys.exit(0)
 
-TODAY_FILE = open(f"{DATA_DIR}/{TODAY_STRING}.json", 'r').read()
-YESTERDAY_FILE = open(f"{DATA_DIR}/{YESTERDAY_STRING}.json", 'r').read()
+    TODAY_FILE = open(f"{DATA_DIR}/{TODAY_STRING}.json", 'r').read()
+    YESTERDAY_FILE = open(f"{DATA_DIR}/{YESTERDAY_STRING}.json", 'r').read()
 
-data = {
-    'yesterday': json.loads(YESTERDAY_FILE)['data'],
-    'today': json.loads(TODAY_FILE)['data']
-}
+    data = {
+        'yesterday': json.loads(YESTERDAY_FILE)['data'],
+        'today': json.loads(TODAY_FILE)['data']
+    }
 
-message = {
-    'content': ""
-}
+    message = {'content': ""}
 
-for dataToday in data['today']:
-    for dataYesterday in data['yesterday']:
-        if dataToday['id'] in dataYesterday['id']:
-            if float(dataYesterday['price']) > float(dataToday['price']):
-                diff = float(dataYesterday['price']) - float(dataToday['price'])
-                message['content'] = f"{message['content']}\n{dataToday['name']} is Cheaper {diff}"
-                print(f"{dataToday['name']} is Cheaper {diff}")
-            elif float(dataYesterday['price']) < float(dataToday['price']):
-                diff = float(dataToday['price']) - float(dataYesterday['price'])
-                message['content'] = f"{message['content']}\n{dataToday['name']} is Expensive {diff}"
-                print(f"{dataToday['name']} is Expensive {diff}")
+    for dataToday in data['today']:
+        for dataYesterday in data['yesterday']:
+            if dataToday['id'] in dataYesterday['id']:
+                if float(dataYesterday['price']) > float(dataToday['price']):
+                    diff = float(dataYesterday['price']) - float(dataToday['price'])
+                    message['content'] = f"{message['content']}\n{dataToday['name']} is Cheaper {diff}"
+                    print(f"{dataToday['name']} is Cheaper {diff}")
+                elif float(dataYesterday['price']) < float(dataToday['price']):
+                    diff = float(dataToday['price']) - float(dataYesterday['price'])
+                    message['content'] = f"{message['content']}\n{dataToday['name']} is Expensive {diff}"
+                    print(f"{dataToday['name']} is Expensive {diff}")
 
-sendMessage = requests.post(WEBHOOK_URL, json=message, headers={"Content-Type": "application/json"})
+    sendMessage = requests.post(WEBHOOK_URL, json=message, headers={"Content-Type": "application/json"})
 
-sys.exit(0)
--- scrapper.py
+++ scrapper.py
@@ -3,38 +3,61 @@ import re
 import json
 import os
 import sys
+import logging
 from datetime import date
 from bs4 import BeautifulSoup
 
-print("Run the Scrapper")
+from notifier import RunNotifier
 
 DATA_DIR = './data'
 TODAY_STRING = date.today().strftime("%Y-%m-%d")
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-req = requests.get("https://www.yogyaonline.co.id/hotdeals.html")
+def scrapper(page=1, limit=80):
+    TARGET_URL = f"https://www.yogyaonline.co.id/hotdeals.html?p={page}&product_list_limit={limit}"
+    logging.info(f"Run the Scrapper page {page} and limit {limit}")
+    req = requests.get(TARGET_URL)
 
-soup = BeautifulSoup(req.text, 'html.parser')
+    parse = BeautifulSoup(req.text, 'html.parser')
 
-p = re.compile('var dlObjects = (.*);')
-found = ""
-for script in soup.find_all("script", {"src":False}):
-    if script:         
-       m = p.search(script.string)
-       if m is not None:
-           found = m.group().replace("var dlObjects =", "")
-           found = found.replace(";", "")
+    p = re.compile('var dlObjects = (.*);')
+    found = ""
+    for script in parse.find_all("script", {"src":False}):
+        if script:
+           m = p.search(script.string)
+           if m is not None:
+               found = m.group().replace("var dlObjects =", "")
+               found = found.replace(";", "")
 
-dataEcommerce = json.loads(found)[0]['ecommerce']
-dataHotDealsProduct = dataEcommerce['impressions']
-compiledData = {
-    'data': dataHotDealsProduct,
-    'date': TODAY_STRING
-}
+    dataEcommerce = json.loads(found)[0]['ecommerce']
+    dataHotDealsProduct = dataEcommerce['impressions']
+    return dataHotDealsProduct
 
-if not os.path.isdir(DATA_DIR):
-    os.mkdir(DATA_DIR)
+if __name__ == "__main__":
+    prevData = {}
+    currData = scrapper()
+    compiledData = []
+    index = 2
+    while not prevData == currData:
+        for item in currData:
+            compiledData.append(item)
+        prevData = currData
+        currData = scrapper(page=index)
+        index += 1
+    
+    cData = {
+        'data': compiledData,
+        'date': TODAY_STRING
+    }
 
-file_object = open(f'{DATA_DIR}/{TODAY_STRING}.json', 'w+')
-file_object.write(json.dumps(compiledData))
+    if not os.path.isdir(DATA_DIR):
+        os.mkdir(DATA_DIR)
 
-sys.exit(0)
+    file_object = open(f'{DATA_DIR}/{TODAY_STRING}.json', 'w+')
+    file_object.write(json.dumps(cData))
+
+    RunNotifier()
+
+    sys.exit(0)
+
+logging.error(f"I'm at {__name__}")
