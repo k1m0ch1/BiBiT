3e80ffffa1352084b839db27614fc9f4e786034a
k1m0ch1 <yahya.kimochi@gmail.com>
2020-12-05 00:13:15 +0700

    add: sent the notification to twitter @bot_bibit

--- notifier.py
+++ notifier.py
@@ -2,15 +2,46 @@ import os
 import sys
 import json
 import requests
-from datetime import date, timedelta
+import logging
+import config
+import time
+import tweepy
+from random import randint
+from datetime import date, timedelta, datetime
+
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 def RunNotifier():
+
     WEBHOOK_URL = "https://discordapp.com/api/webhooks/781911618702409748/QQ89_tlwWYuT-fUSEkUnPMmJDlH2ohn6rp8bbcIEhL2cHX_8fbLdtE5UGLZ4VbU4fjnR"
-    print("Run the Notifier")
+    logging.info("Run the Notifier")
 
     DATA_DIR = './data'
     TODAY_STRING = date.today().strftime("%Y-%m-%d")
     YESTERDAY_STRING = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+    def CharacterLimit(platform=str, message=str, data=list) -> list:
+        CONFIGURATION = (len(data[len(data)-1])+len(message)) < 2000 if platform == "discord" else \
+            (len(data[len(data)-1])+len(message)) < 200
+        if CONFIGURATION:
+            data[len(data)-1] += message
+        else:
+            data += [message]
+
+        return data
+
+    def SendMessage(platform=str, dataToday=dict, dataYesterday=dict) -> str:
+        ori_diff = float(dataYesterday['price']) - float(dataToday['price'])
+        diff = ori_diff * -1 if ori_diff < 0 else ori_diff
+        cheap = True if ori_diff > 0 else False
+        logging.info(f"{dataToday['name']} is {'Cheaper' if cheap else 'Expensive'} {diff}")
+        promotion = f"{dataToday['promotion']['type']} {dataToday['promotion']['description']}" if "promotion" in dataToday else ""
+        if platform == "discord":
+            return f"\n[{dataToday['name']}]({dataToday['link']}) is {'Cheaper' if cheap else 'Expensive'} {diff} {promotion}"
+        return f"{dataToday['name']} is {'Cheaper' if cheap else 'Expensive'} {diff} {promotion}" \
+            f" YESTERDAY({dataYesterday['price']}) - TODAY({dataToday['price']}) = {ori_diff} {'Cheaper' if cheap else 'Expensive'}" \
+            f" {dataToday['link']}"
+
 
     if not os.path.isfile(f'{DATA_DIR}/{YESTERDAY_STRING}.json'):
         print("There is no yesterday data file, you need to wait more")
@@ -24,37 +55,37 @@ def RunNotifier():
         'today': json.loads(TODAY_FILE)['data']
     }
 
-    item_message = ["Yogya Bot Price Today!\n Formula Yesterday - Today = Cheaper, Today - Yesterday = Expensive"]
+    discord_message = ["Yogya Bot Price Today!\n Formula Yesterday - Today = Cheaper, Today - Yesterday = Expensive"]
+    twitter_message = [""]
 
     for dataToday in data['today']:
         for dataYesterday in data['yesterday']:
             if dataToday['id'] in dataYesterday['id']:
-                if float(dataYesterday['price']) > float(dataToday['price']):
-                    diff = float(dataYesterday['price']) - float(dataToday['price'])
-                    curr_message = f"\n[{dataToday['name']}]({dataToday['link']}) is Cheaper {diff}"
-                    curr_message += f" {dataToday['promotion']['type']} {dataToday['promotion']['description']}" if "promotion" in dataToday else ""
-                    #curr_message += f"--{dataYesterday['price']} - {dataToday['price']} = {diff} CHEAPER"
-                    if len(item_message) == 0:
-                        item_message.append(curr_message)
-                    if (len(item_message[len(item_message)-1])+len(curr_message)) < 2000:
-                        item_message[len(item_message)-1] += curr_message
-                    else:
-                        item_message.append(curr_message)
-                    print(f"{dataToday['name']} is Cheaper {diff}")
-                elif float(dataYesterday['price']) < float(dataToday['price']):
-                    diff = float(dataToday['price']) - float(dataYesterday['price'])
-                    curr_message = f"\n[{dataToday['name']}]({dataToday['link']}) is Expensive {diff}"
-                    curr_message += f" {dataToday['promotion']['type']} {dataToday['promotion']['description']}" if "promotion" in dataToday else ""
-                    #curr_message += f"\n||{dataToday['price']} - {dataYesterday['price']} = {diff} EXPENSIVE||"
-                    if len(item_message) == 0:
-                        item_message.append(curr_message)
-                    if (len(item_message[len(item_message)-1])+len(curr_message)) < 2000:
-                        item_message[len(item_message)-1] += curr_message
-                    else:
-                        item_message.append(curr_message)
-                    print(f"{dataToday['name']} is Expensive {diff}")
+                if float(dataYesterday['price']) - float(dataToday['price']) == 0:
+                    continue
 
-    for item in item_message:
-        sendMessage = requests.post(WEBHOOK_URL, json={'content': item}, headers={"Content-Type": "application/json"})
+                curr_message = {
+                    "discord": SendMessage("discord", dataToday, dataYesterday),
+                    "twitter": SendMessage("twitter", dataToday, dataYesterday)
+                }
+
+                discord_message = CharacterLimit("discord", curr_message['discord'], discord_message)
+                twitter_message = CharacterLimit("twitter", curr_message['twitter'], twitter_message)
+
+    for message in discord_message:
+        sendMessage = requests.post(WEBHOOK_URL, json={'content': message}, headers={"Content-Type": "application/json"})
         if not sendMessage.status_code == 204:
-            print(f"ERROR, with status code , the message is not sent with value {message}")
+            logging.error(f"ERROR, with status code , the message is not sent with value {message}")
+    
+    auth = tweepy.OAuthHandler(config.TWITTER_API_KEY, config.TWITTER_API_SECRET_KEY)
+    auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    parent = api.update_status(f"Yogya Bot Price Today! {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+    parent_id = parent.id_str
+    logging.info("Parent Message is created")
+    for message in twitter_message:
+        randomize = randint(4, 10)
+        logging.info(f"Sent Message to Thread {parent_id} with pause {randomize} second")
+        time.sleep(randomize)
+        api.update_status(message, parent_id)
