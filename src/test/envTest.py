5d193ec6e14bba1cf49f88739606bf2157ef1b99
k1m0ch1 <yahya.kimochi@gmail.com>
2020-12-13 12:59:35 +0700

    add: verbose and much propoer action

--- src/test/envTest.py
+++ src/test/envTest.py
@@ -5,11 +5,13 @@ class TestEnvironmentVariable(unittest.TestCase):
     def testEnvVarNotEmpty(self):
         from src import config
         if config.TWITTER_NOTIFICATION:
+            print(f"twitter notification is enabled")
             assert len(config.TWITTER_API_KEY) > 0, "env TWITTER_API_KEY is empty"
             assert len(config.TWITTER_API_SECRET_KEY) > 0, "env TWITTER_API_SECRET_KEY is empty"
             assert len(config.TWITTER_ACCESS_TOKEN) > 0, "env TWITTER_ACCESS_TOKEN is empty"
             assert len(config.TWITTER_ACCESS_TOKEN_SECRET) > 0, "env TWITTER_ACCESS_TOKEN_SECRET is empty"
         if config.DISCORD_NOTIFICATION:
+            print(f"discord notification is enabled")
             assert len(config.DISCORD_WEBHOOK_URL) > 0, "env DISCORD_WEBHOOK_URL is empty"
 
 if __name__ == '__main__':
