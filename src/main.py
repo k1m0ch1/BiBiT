6c57a11995f526bb6b8c729a2f263e04ab040828
k1m0ch1 <yahya.kimochi@gmail.com>
2020-12-26 14:49:27 +0700

    change: cleanup the main py

--- src/main.py
+++ src/main.py
@@ -15,16 +15,11 @@ from config import DATA_DIR
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-
-def jobScrapper(target: str = 'all'):
+def dataScrap(target: str):
     TODAY_STRING = date.today().strftime("%Y-%m-%d")
-    YESTERDAY_STRING = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
-
-    logging.info("=== worker is running")
     prevData = {}
-
-    if target in ['yogyaonline', 'all']:
-        logging.info("=== start scrapping yogya")
+    cData = {}
+    if target == 'yogyaonline':
         currData = hotDealsPage()
         compiledData = []
         index = 2
@@ -34,57 +29,41 @@ def jobScrapper(target: str = 'all'):
             prevData = currData
             currData = hotDealsPage(page=index)
             index += 1
+        cData = {'data': compiledData, 'date': TODAY_STRING }
+    
+    if target == 'klikindomaret':
+        cData = {'data': promosiMingguIni(), 'date': TODAY_STRING }
 
-        cData = {
-            'data': compiledData,
-            'date': TODAY_STRING
-        }
+    if target == 'alfacart':
+        cData = {'data': promotion(), 'date': TODAY_STRING }
 
-        if not os.path.isdir(f"{DATA_DIR}/yogyaonline"):
-            os.mkdir(f"{DATA_DIR}/yogyaonline")
+    if not os.path.isdir(f"{DATA_DIR}/{target}"):
+        os.mkdir(f"{DATA_DIR}/{target}")
 
-        file_object = open(f'{DATA_DIR}/yogyaonline/{TODAY_STRING}.json', 'w+')
-        file_object.write(json.dumps(cData))
+    file_object = open(f'{DATA_DIR}/{target}/{TODAY_STRING}.json', 'w+')
+    file_object.write(json.dumps(cData))
 
-        logging.info(f"== scrapping success, saved to {DATA_DIR}/{TODAY_STRING}.json")
+    logging.info(f"== scrapping {target} success, saved to {DATA_DIR}/{target}/{TODAY_STRING}.json")
 
-    if target in ['klikindomaret', 'all']:
-        logging.info("=== start scrapping indomaret")    
-        cData = {
-            'data': promosiMingguIni(),
-            'date': TODAY_STRING
-        }
+def jobScrapper(target: str = 'all'):
+    logging.info("=== worker is running")
 
-        if not os.path.isdir(f"{DATA_DIR}/indomaret"):
-            os.mkdir(f"{DATA_DIR}/indomaret")
-
-        file_object = open(f'{DATA_DIR}/indomaret/{TODAY_STRING}.json', 'w+')
-        file_object.write(json.dumps(cData))
-
-        logging.info(f"== scrapping indomaret success, saved to {DATA_DIR}/indomaret/{TODAY_STRING}.json")
-
-    if target in ['alfacart', 'all']:
-        logging.info("=== start scrapping alfacart")    
-        cData = {
-            'data': promotion(),
-            'date': TODAY_STRING
-        }
-
-        if not os.path.isdir(f"{DATA_DIR}/alfacart"):
-            os.mkdir(f"{DATA_DIR}/alfacart")
-
-        file_object = open(f'{DATA_DIR}/alfacart/{TODAY_STRING}.json', 'w+')
-        file_object.write(json.dumps(cData))
-
-        logging.info(f"== scrapping alfacart success, saved to {DATA_DIR}/alfacart/{TODAY_STRING}.json")
-
+    if target == 'all':
+        for itemTarget in ['yogyaonline', 'alfacart', 'klikindomaret']:
+            job = dataScrap(itemTarget)
+            if not job:
+                log.Println("Fail Scrapping")
+    else:
+        job = dataScrap(target)
+        if not job:
+            log.Println("Fail Scrapping")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process the BiBiT Job')
     parser.add_argument('command', 
         metavar ='command', 
         type=str, 
-        choices=['notification', 'scrapping', 'do.notif', 'do.scrap'],
+        choices=['notif', 'scrap', 'do.notif', 'do.scrap'],
         help='a command to run the bibit Job')
     
     parser.add_argument('--target', 
@@ -96,12 +75,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.command == 'notification':
+    if args.command == 'notif':
         for PRIME_TIME in config.PRIME_TIME:
             logging.info(f"=== notification worker at {PRIME_TIME} is queued")
             schedule.every().day.at(PRIME_TIME).do(runNotifier)
 
-    if args.command == 'scrapping':
+    if args.command == 'scrap':
         for SCRAPPING_TIME in config.SCRAPPING_TIME:
             logging.info(f"=== scrapper worker at {SCRAPPING_TIME} is queued")
             schedule.every().day.at(SCRAPPING_TIME).do(jobScrapper)
