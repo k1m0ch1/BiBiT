33a2eb1e1e4db2c2b4610ede671d02dfbb0226e1
k1m0ch1 <yahya.kimochi@gmail.com>
2021-10-13 22:55:31 +0700

    hotfix: give an error when file not found

--- src/routes/root.py
+++ src/routes/root.py
@@ -2,6 +2,7 @@ from config import DATA_DIR
 from fastapi import APIRouter
 from datetime import date
 from typing import Optional
+import os 
 
 import json
 
@@ -9,7 +10,10 @@ root = APIRouter()
 
 @root.get("/")
 async def index(start: Optional[int] = 0, end: Optional[int] = 50, date: Optional[str] = date.today().strftime("%Y-%m-%d")):
-    TARGETDB = json.loads(open(f"{DATA_DIR}/yogyaonline/catalog/{date}.json", 'r').read())['data']
+    FILENAME = f"{DATA_DIR}/yogyaonline/catalog/{date}.json"
+    if not os.path.exists(FILENAME):
+        return {"message": f"the data on this date is not exist"}
+    TARGETDB = json.loads(open(FILENAME, 'r').read())['data']
     return {
         "message": f"Success fetch data",
         "data": TARGETDB[start:end]
