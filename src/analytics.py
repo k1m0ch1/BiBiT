d6e523a6ccb069efc9cd5e7ad95763afe0b64185
k1m0ch1 <yahya.kimochi@gmail.com>
2021-10-14 13:18:37 +0700

    fix: more efficient to generate analytic data

--- src/analytics.py
+++ src/analytics.py
@@ -10,38 +10,36 @@ def genAnalytic():
         print(target)
         getLatestDate = date.today().strftime("%Y-%m-%d")
         FILE_NAME = f'./{target}-analytics.json'
-        dataCompile = {"latest_date": ""}
+        dataCompile = {"gathered_date": []}
         if os.path.exists(FILE_NAME):
             file_object = open(FILE_NAME).read()
             data = json.loads(file_object)
-            if "latest_date" in data:
-                getLatestDate = json.loads(file_object)["latest_date"]
+            if "gathered_date" in data:
+                getLatestDate = json.loads(file_object)["gathered_date"]
             dataCompile = json.loads(file_object)
         else:
             file_object = open(f'./{target}-analytics.json', 'w+')
-            file_object.write(json.dumps({"latest_date": ""}))
+            file_object.write(json.dumps({"gathered_date": []}))
             file_object.close()
 
-        for tipe in ['catalog']:
+        for tipe in ['catalog', 'promo']:
             print(f"Generate data {tipe}")
             dirr = f"{config.DATA_DIR}/{target}/{tipe}"
             listfiles = [filename for filename in listdir(dirr) if isfile(join(dirr, filename))]
             print("I will merge all of this file")
 
-            for filename in listfiles:
+            for filename in listfiles[:20]:
 
                 file_object = open(FILE_NAME).read()
                 data = json.loads(file_object)
                 getLatestDate = date.today().strftime("%Y-%m-%d")
-                if "latest_date" in data:
-                    getLatestDate = "1990-10-10" if data["latest_date"] == "" else data["latest_date"]
+                if "gathered_date" in data:
+                    getLatestDate = data["gathered_date"]
                 dataCompile = json.loads(file_object)
 
                 NOW = filename.split(".json")[0]
-                NOWDATE = datetime.strptime(NOW, '%Y-%m-%d')
-                LASTDATE = datetime.strptime(getLatestDate, '%Y-%m-%d')
 
-                if NOWDATE <= LASTDATE and getLatestDate != "":
+                if NOW in getLatestDate:
                     print(f"File {dirr}/{filename} already gathered, skipped")
                     continue
                 
@@ -55,7 +53,8 @@ def genAnalytic():
                             NOW: dataToday['price']
                         }
                     dataCompile[dataToday['name']][each_file['date']] = dataToday['price']
-                    dataCompile["latest_date"] = NOW
+                    if NOW not in dataCompile["gathered_date"]:
+                        dataCompile["gathered_date"].append(NOW)
 
                 file_object = open(f'./{target}-analytics.json', 'w+')
                 file_object.write(json.dumps(dataCompile))
