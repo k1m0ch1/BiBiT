e04c11190cd3d6bff70548968ad6790dcbadb088
k1m0ch1 <yahya.kimochi@gmail.com>
2024-03-08 00:07:33 +0700

    feat: fixing scrapper

--- src/crawler/yogyaonline.py
+++ src/crawler/yogyaonline.py
@@ -1,18 +1,26 @@
 import requests
 import re
 import json
-import os
-import sys
 import logging
+import sys
 from bs4 import BeautifulSoup
-from config import DATA_DIR, HEADERS
+from config import HEADERS
 from fp.fp import FreeProxy
+from datetime import datetime
+import pytz
+import shortuuid
+from util import cleanUpCurrency
+
+from db import DBSTATE
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
+db = DBSTATE
 
 def scrap(URL, index):
-    TARGET_URL = f"{URL}?p={index}&product_list_limit=80"
+    # understand the item limit, right now upto 640
+    # is it possible to make the limit bigger like 9999
+    TARGET_URL = f"{URL}?p={index}&product_list_limit=640"
     dataProduct = []
     list_ids = []
     # logging.info(f"Run the Scrapper {URL} page {index}")
@@ -21,8 +29,11 @@ def scrap(URL, index):
     parser = BeautifulSoup(req.text, 'html.parser')
 
     productLink = [item['href'] for item in parser.find_all("ol", {"class": "products list items product-items"})[0].find_all("a", {"class": "product-item-link"}, href=True)]
+
+    print(len(productLink))
     productImages = [item.get('data-original') for item in parser.find_all("img", {"class": "product-image-photo lazy"})]
     promotion = []
+    
     for item in parser.find_all("div", {"class": "price-box price-final_price"}):
         tType = ""
         description = ""
@@ -48,22 +59,23 @@ def scrap(URL, index):
                 "original_price": ""
             })
 
-    p = re.compile('var dlObjects = (.*);')
+    p = re.compile('var dl4Objects = (.*);')
     found = ""
     for script in parser.find_all("script", {"src":False}):
         if script:
             m = p.search(script.string)
             if m is not None:
-                found = m.group().replace("var dlObjects =", "")
+                found = m.group().replace("var dl4Objects =", "")
                 found = found.replace(";", "")
     
     data_raw = json.loads(found)
+
     for index, grouped_item in enumerate(data_raw):
         dataEcommerce = grouped_item['ecommerce']
-        for item in dataEcommerce['impressions']:
-            if item['id'] not in list_ids:
+        for item in dataEcommerce['items']:
+            if item['item_id'] not in list_ids:
                 dataProduct.append(item)
-                list_ids.append(item['id'])
+                list_ids.append(item['item_id'])
             else:
                 logging.info(f"{item['name']} already exist")
                 productLink.pop(index)
@@ -72,6 +84,19 @@ def scrap(URL, index):
         dataProduct[key]['link'] = value
         dataProduct[key]['promotion'] = promotion[key]
         dataProduct[key]['image'] = productImages[key]
+        now = datetime.now(pytz.timezone("Asia/Jakarta"))
+        checkIdItem = db.select(TABLE='items', SELECT='id', WHERE=(db['items']['sku'] == dataProduct[key]['item_id']) | (db['items']['name'] == dataProduct[key]['item_name']))
+        if len(checkIdItem) > 0:
+            idItem = checkIdItem[0][0]
+            # harus di cek lagi, hari ini datanya sama atau tidak
+            # kalau hari ini datanya sama, ga usah simpen data harga sama diskon
+            db["prices"].insert(shortuuid.uuid(), idItem, cleanUpCurrency(dataProduct[key]['price']), "", now.strftime("%Y-%m-%d %H:%M:%S"))
+            db["discounts"].insert(shortuuid.uuid(), idItem, dataProduct[key]['price'], cleanUpCurrency(dataProduct[key]['promotion']['original_price']), dataProduct[key]['promotion']['description'], "", now.strftime("%Y-%m-%d %H:%M:%S"))
+        else:
+            idItem = shortuuid.uuid()
+            db["items"].insert(idItem, dataProduct[key]['item_id'], dataProduct[key]['item_name'], dataProduct[key]['item_list_name'], dataProduct[key]['image'], dataProduct[key]['link'], 'yogyaonline', now.strftime("%Y-%m-%d %H:%M:%S"))
+            db["prices"].insert(shortuuid.uuid(), idItem, cleanUpCurrency(dataProduct[key]['price']), "", now.strftime("%Y-%m-%d %H:%M:%S"))
+            db["discounts"].insert(shortuuid.uuid(), idItem, dataProduct[key]['price'], cleanUpCurrency(dataProduct[key]['promotion']['original_price']), dataProduct[key]['promotion']['description'], "", now.strftime("%Y-%m-%d %H:%M:%S"))
 
     return dataProduct
 
@@ -83,7 +108,11 @@ def getCategories():
     categoryClass = parser.find_all("li", {"id": re.compile('vesitem-*')})
     categories = []
     for cat in categoryClass:
-        if cat.find('a').get('href') == "https://www.yogyaonline.co.id/blog-yogya-online":
+        if cat.find('a').get('href') == "https://yogyaonline.co.id/blog-yogya-online":
+            continue
+        if cat.find('a').get('href') == "https://yogyaonline.co.id/hotdeals.html":
+            continue
+        if cat.find('a').get('href') == "#":
             continue
         if cat.find('a').get('href') not in categories:
             categories.append(cat.find('a').get('href'))
@@ -93,7 +122,6 @@ def getCategories():
     for index, category in enumerate(categories):
         logging.info(f"get {category} from {index+1}/{len(categories)}")
         prevData = {}
-        cData = {}
         index = 2
         currData = scrap(category, 1)
         compiledData += currData
@@ -103,6 +131,9 @@ def getCategories():
             currData = scrap(category, index)
             index += 1
             compiledData += currData
+        
+        sys.exit()
+
     return compiledData
     
     
